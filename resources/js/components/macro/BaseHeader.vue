a25de4ef235872516e1ed136d51012210bad4060
Evanflow89 <ivanbaragone@gmail.com>
2022-07-27 18:24:51 +0200

    Added Macros

---
+++ resources/js/components/macro/BaseHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h1>Boolpress</h1>
+        </div>
+        <div class="col-6"></div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "BaseHeader",
+};
+</script>
+
+<style>
+</style>
