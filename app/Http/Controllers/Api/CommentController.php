45cdbbe506fb61d15226dde73dc76a8dd0099591
Evanflow89 <ivanbaragone@gmail.com>
2022-08-04 17:21:05 +0200

    Add: validate Comment Controller

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -11,13 +11,18 @@ class CommentController extends Controller
     public function store(Request $request, $post_id, )
     {
              //prendo i dati dalla request
-            $data = $request->all();
-             //faccio una validazione
+            $request->validate([
+                'name' => 'nullable|string|max:80',
+                'content' => 'required|string',
+            ]);
 
+            $data =$request->all();
+            $data['post_id'] = $post_id;
+             
 
              //creo il commento nel Database usando il model
             $newComment = new Comment();
-            $newComment->post_id = $post_id;
+            $newComment->post_id = $data['post_id'];
             $newComment->name = $data['name'];
             $newComment->content = $data['content'];
             $newComment->save();
